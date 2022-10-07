b8a77445358ebf95c760119221fbe5a5072c31b9
chidanandkhot <chidukhot@gmail.com>
2022-10-07 16:01:50 +0530

    Login test

---
+++ com/vilcart/util/AngularWait.java
@@ -0,0 +1,15 @@
+package com.vilcart.util;
+
+public class AngularWait {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void waitAllRequest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
